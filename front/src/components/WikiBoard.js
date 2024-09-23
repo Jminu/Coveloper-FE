c195e724b9f070dcc90694b6e33a754929901e5f
Jin Minu <s9430939@naver.com>
2024-09-23 18:44:31 +0900

    css

--- front/src/components/WikiBoard.js
+++ front/src/components/WikiBoard.js
@@ -65,26 +65,28 @@ function WikiBoard({ teamId }) {
     <div className={styles["wiki-board-container"]}>
       <h3>{title}</h3>
       {editMode ? (
-        <div className={styles["editor-and-preview"]}>
-          <div className={styles["editor"]}>
-            <textarea
-              value={wikiContent}
-              onChange={(e) => setWikiContent(e.target.value)} // 실시간 업데이트
-              placeholder="문서 내용을 입력하세요..."
-            ></textarea>
-          </div>
-          <div className={styles["preview"]}>
-            <Markdown
-              options={{
-                overrides: {
-                  code: {
-                    component: CodeBlock, // 커스텀 코드 블록 렌더러
+        <div>
+          <div className={styles["editor-and-preview"]}>
+            <div className={styles["editor"]}>
+              <textarea
+                value={wikiContent}
+                onChange={(e) => setWikiContent(e.target.value)} // 실시간 업데이트
+                placeholder="문서 내용을 입력하세요..."
+              ></textarea>
+            </div>
+            <div className={styles["preview"]}>
+              <Markdown
+                options={{
+                  overrides: {
+                    code: {
+                      component: CodeBlock, // 커스텀 코드 블록 렌더러
+                    },
                   },
-                },
-              }}
-            >
-              {wikiContent}
-            </Markdown>
+                }}
+              >
+                {wikiContent}
+              </Markdown>
+            </div>
           </div>
           <button onClick={handleSavePage}>저장</button>
         </div>
